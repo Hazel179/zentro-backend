b218438b99edcdf2646fc2557f7c544706aa5767
Hazel <khaingnyein1701@gmail.com>
2025-07-19 01:59:24 +0400

    Zentro backend initial commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const serviceRoutes = require('./routes/services');
 const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000 ;
 
 // Connect to MongoDB
 connectDB();
